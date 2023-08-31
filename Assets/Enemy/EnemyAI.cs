a080c380457b844fb66c6ddec5f417706423d55c
BuildTools <93136063+300f20t@users.noreply.github.com>
2023-08-31 05:25:55 +0300

    Update EnemyAI.cs

--- Assets/Enemy/EnemyAI.cs
+++ Assets/Enemy/EnemyAI.cs
@@ -1,59 +1,39 @@
 using UnityEngine;
-using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
+    public float movementSpeed = 2f;
     public float detectionRange = 5f;
     public float stoppingDistance = 1f;
 
     private GameObject player;
-    private NavMeshAgent navMeshAgent;
     private bool playerDetected = false;
 
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-
-        // Добавляем компонент NavMeshAgent, если его ещё нет
-        if (!TryGetComponent<NavMeshAgent>(out navMeshAgent))
-        {
-            navMeshAgent = gameObject.AddComponent<NavMeshAgent>();
-            navMeshAgent.enabled = false; // Отключаем его по умолчанию
-        }
     }
 
     private void Update()
     {
-        if (player != null)
+        if (player != null && !playerDetected)
         {
             float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
 
-            if (!playerDetected && distanceToPlayer <= detectionRange)
+            if (distanceToPlayer <= detectionRange)
             {
                 playerDetected = true;
-                navMeshAgent.enabled = true; // Включаем NavMeshAgent при обнаружении игрока
-                navMeshAgent.SetDestination(player.transform.position);
             }
+        }
 
-            if (playerDetected)
+        if (playerDetected)
+        {
+            if (player != null)
             {
-                if (distanceToPlayer > detectionRange)
-                {
-                    playerDetected = false;
-                    navMeshAgent.enabled = false; // Отключаем NavMeshAgent, если игрок вышел из диапазона
-                }
-                else
-                {
-                    if (distanceToPlayer <= stoppingDistance)
-                    {
-                        navMeshAgent.isStopped = true; // Останавливаем агента, когда он близко к игроку
-                    }
-                    else
-                    {
-                        navMeshAgent.isStopped = false;
-                    }
-                }
+                Vector3 directionToPlayer = (player.transform.position - transform.position).normalized;
+                transform.position += directionToPlayer * movementSpeed * Time.deltaTime;
             }
         }
     }
+
 }
