181c78bf74007093218f8ad775fe6109b4ca11be
BuildTools <93136063+300f20t@users.noreply.github.com>
2023-09-01 03:12:34 +0300

    Update Coin.cs

--- Assets/Coin.cs
+++ Assets/Coin.cs
@@ -5,6 +5,7 @@ public class Coin : MonoBehaviour
     private PlayerCoins playerCoins;
     public float attractionForce = 5f; // Сила притяжения
     public float minDistanceToPlayer = 1f; // Минимальное расстояние до игрока, при котором монета будет удалена
+    public float maxDistanceToPlayer = 10f; // Максимальное расстояние до игрока, на котором действует притяжение
 
     private Transform player;
 
@@ -27,7 +28,7 @@ public class Coin : MonoBehaviour
                 // Монета близко к игроку, удаляем её
                 Destroy(gameObject);
             }
-            else
+            else if (distanceToPlayer < maxDistanceToPlayer)
             {
                 // Притяжение монеты к игроку
                 Vector3 attractionDirection = directionToPlayer.normalized;
