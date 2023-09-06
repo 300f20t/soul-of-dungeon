79a50444d97454ca1558ee8b3a91a13bc426deba
BuildTools <93136063+300f20t@users.noreply.github.com>
2023-09-06 23:38:51 +0300

    Update ZoneHider.cs

--- Assets/UI/ZoneHider.cs
+++ Assets/UI/ZoneHider.cs
@@ -1,26 +1,26 @@
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
 
-public class ZoneHider : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class ZoneHider : MonoBehaviour
 {
-    [SerializeField] private GameObject[] objectsToShow; // Массив объектов для отображения
+    [SerializeField] private Vector3 maxPosition; // Максимальная позиция
+    [SerializeField] private Vector3 minPosition; // Минимальная позиция
+    [SerializeField] private RectTransform objectToHide; // Используем RectTransform
 
-    // Вызывается, когда мышь входит в зону
-    public void OnPointerEnter(PointerEventData eventData)
+    private void Update()
     {
-        foreach (GameObject obj in objectsToShow)
+        Vector3 objectPosition = objectToHide.anchoredPosition; // Получаем позицию RectTransform
+
+        // Проверяем, находится ли объект внутри заданной зоны
+        if (objectPosition.x >= minPosition.x && objectPosition.x <= maxPosition.x &&
+            objectPosition.y >= minPosition.y && objectPosition.y <= maxPosition.y)
         {
-            obj.SetActive(true); // Отображаем объекты
+            // Объект входит в зону, делаем его активным
+            objectToHide.gameObject.SetActive(true);
         }
-    }
-
-    // Вызывается, когда мышь выходит из зоны
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        foreach (GameObject obj in objectsToShow)
+        else
         {
-            obj.SetActive(false); // Скрываем объекты
+            // Объект покидает зону, делаем его неактивным
+            objectToHide.gameObject.SetActive(false);
         }
     }
 }
