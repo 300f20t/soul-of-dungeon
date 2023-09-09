541fde53a259406457b133042528ae069fa4d631
BuildTools <93136063+300f20t@users.noreply.github.com>
2023-09-09 03:06:50 +0300

    DisccordRPC доюавленно

--- Assets/UI/Menu/ExitGame.cs
+++ Assets/UI/Menu/ExitGame.cs
@@ -1,9 +1,17 @@
+using System;
+using UnityEditor.PackageManager;
 using UnityEngine;
+using DiscordRPC;
 
 public class ExitGame : MonoBehaviour
 {
     public void Exit()
     {
+        var client = new DiscordRpcClient("1138637051349188710");
+
+        // Завершите соединение перед выходом
+        client.Dispose();
+
         Application.Quit(); // Завершаем приложение при нажатии Esc
     }
 }
